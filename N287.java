e9d59b8465b5724c2f7aee463634ff6734d78148
Immortalfires <2247797673@qq.com>
2019-10-24 22:36:20 +0800

    桶排序思想 O(1)额外空间不知道啥意思

---
+++ N287.java
@@ -0,0 +1,40 @@
+/*287
+给定一个包含 n + 1 个整数的数组 nums，其数字都在 1 到 n 之间（包括 1 和 n）
+可知至少存在一个重复的整数。假设只有一个重复的整数，找出这个重复的数。
+示例 1:
+        输入: [1,3,4,2,2]
+        输出: 2
+示例 2:
+        输入: [3,1,3,4,2]
+        输出: 3
+
+说明：不能更改原数组（假设数组是只读的）。
+     只能使用额外的 O(1) 的空间。
+     时间复杂度小于 O(n2) 。
+     数组中只有一个重复的数字，但它可能不止重复出现一次。
+
+
+桶排序的思想
+ */
+
+public class N287 {
+    public static void main(String[] args) {
+        int nums[] = new int[]{1,3,4,2,2};
+        int ans = findDuplicate(nums);
+        System.out.println(ans);
+    }
+
+    public static int findDuplicate(int[] nums) {
+        int res = 0;
+        int result[] = new int[nums.length];
+        for(int i:nums){
+            result[i]++;
+        }
+        for(int j = 0;j < result.length;j++){
+            if(result[j] > 1){
+                res = j;
+            }
+        }
+        return res;
+    }
+}
